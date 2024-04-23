165c5a9a1a485ae44b34fb8a34346b98398c791c
ivanberez <berejnoyi@mail.ru>
2024-04-23 11:14:45 +0800

    Changes after 1 attempts

--- Assets/Scripts/PlayerCollisionHandlers.cs
+++ Assets/Scripts/PlayerCollisionHandlers.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 
 public class PlayerCollisionHandlers : MonoBehaviour
 {
-    public event Action<Player> EnemyCollideHandler;
+    public event Action<Enemy> EnemyCollideHandler;
     public event Action<Coin> CoinCollideHandler;
 
     [field:SerializeField] public CollidCheker GroundCheker { get; private set; }                       
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.transform.TryGetComponent(out Player player))
-            EnemyCollideHandler?.Invoke(player);        
+        if(collision.transform.TryGetComponent(out Enemy enemy))
+            EnemyCollideHandler?.Invoke(enemy);        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
