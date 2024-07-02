cbe7b34996cf1441d4f462e6514c91bdbcd5a53d
ivanberez <berejnoyi@mail.ru>
2024-07-02 16:46:44 +0800

    Modification

--- Assets/Scripts/Player/InputHandler.cs
+++ Assets/Scripts/Player/InputHandler.cs
@@ -5,32 +5,24 @@ using UnityEngine;
 public class InputHandler : MonoBehaviour
 {
     private const string Horizontal = nameof(Horizontal);
+    private const KeyCode JumpKey = KeyCode.Space;
 
-    private Rigidbody2D _rigidbody2D;
-    private bool _isSpaceDown;
-    
+    private Rigidbody2D _rigidbody2D;    
+
     public event Action MouseDownEvent;
+    public event Action JumpKeyDownEvent;
 
     public float HorizontalAxis { get; private set; }
-    public float VerticalAxis => _rigidbody2D.velocity.y;
-    public bool IsSpaceDown
-    {
-        get
-        {
-            bool result = _isSpaceDown;
-            _isSpaceDown = false;
-            return result;
-        }
-    }
+    public float VerticalAxis => _rigidbody2D.velocity.y;    
 
     private void Awake() => _rigidbody2D = GetComponent<Rigidbody2D>();
 
     private void Update()
     {
-        HorizontalAxis = Input.GetAxis(Horizontal);        
-       
-        if(Input.GetKeyDown(KeyCode.Space)) 
-            _isSpaceDown = true;
+        HorizontalAxis = Input.GetAxis(Horizontal);
+
+        if (Input.GetKeyDown(JumpKey))
+            JumpKeyDownEvent?.Invoke();
 
         if (Input.GetMouseButtonDown(0))
             MouseDownEvent?.Invoke();
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 [RequireComponent(typeof(AnimationChanger), typeof(CollisionTransmitter), typeof(Wallet))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private VampirSkill _vampirSkill;
-
     private Mover _mover;
     private Health _health;
     private InputHandler _input;
     private CollisionTransmitter _collisionTransmitter;
+    
+    private bool _isCanJump;
 
     private void Awake()
     {
@@ -21,27 +21,41 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (_input.HorizontalAxis != 0)
-            _mover.Move(_input.HorizontalAxis);
-
-        if (_input.IsSpaceDown && _collisionTransmitter.GroundCheker.IsGrounded)
-            _mover.Jump();
-    }
+        Move();                
+        Jump();
+    }    
 
     private void OnEnable()
     {
         _collisionTransmitter.EnemyCollision += TakeDamage;
         _collisionTransmitter.AidKitCollision += TakeAidKit;
         _health.Ending += Death;
-        _vampirSkill.Impacted += _health.Add;
+        _input.JumpKeyDownEvent += DownJumpKey;
     }
 
     private void OnDisable()
     {
         _collisionTransmitter.EnemyCollision -= TakeDamage;
+        _collisionTransmitter.AidKitCollision -= TakeAidKit;
         _health.Ending -= Death;
-        _vampirSkill.Impacted -= _health.Add;
+        _input.JumpKeyDownEvent += DownJumpKey;
     }
+
+    private void Move()
+    {
+        _mover.Move(_input.HorizontalAxis);
+    }
+
+    private void Jump()
+    {
+        if (_isCanJump)
+        {
+            _mover.Jump();
+            _isCanJump = false;
+        }
+    }
+
+    private void DownJumpKey() => _isCanJump = _collisionTransmitter.GroundCheker.IsGrounded;
 
     private void TakeAidKit(AidKit kit)
     {
@@ -58,5 +72,5 @@ public class Player : MonoBehaviour
     private void Death()
     {
         Destroy(gameObject);
-    }
+    }    
 }
